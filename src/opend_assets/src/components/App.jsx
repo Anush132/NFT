622c99ec4bbfac1e5501e50b7255baf6928abbca
Anush132 <116074251+Anush132@users.noreply.github.com>
2023-04-23 22:06:49 +0530

    Add files via upload

---
+++ src/opend_assets/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import homeImage from "../../assets/home-img.png";
+import Item from "./Item"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Item/>
+      {/* <img className="bottom-space" src={homeImage} /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
